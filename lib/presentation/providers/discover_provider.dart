1bb5f74b3331bbeb4f8e629c9331cac9a5ecd882
fermolanoc <fm87usa@gmail.com>
2025-01-18 13:29:41 -0500

    Add initial Discover screen and provider

---
+++ lib/presentation/providers/discover_provider.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+import '../../domain/entities/video_post.dart';
+
+class DiscoverProvider extends ChangeNotifier {
+  bool initialLoading = true;
+  List<VideoPost> videos = [];
+
+  Future<void> loadNextPage() async {
+    // TODO: implement loadNextPage
+
+    notifyListeners();
+  }
+}
