ecf1fd53dcc0540b0941e9a182809f690ebb488b
fermolanoc <fm87usa@gmail.com>
2025-01-18 17:21:28 -0500

    Add VideoPost Scrollable view + views and likes icons

---
+++ lib/presentation/Widgets/shared/video_scrollable_view.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:toktik/domain/entities/video_post.dart';
+import 'package:toktik/presentation/Widgets/shared/video_buttons.dart';
+
+class VideoScrollableView extends StatelessWidget {
+  final List<VideoPost> videos;
+  const VideoScrollableView({super.key, required this.videos});
+
+  @override
+  Widget build(BuildContext context) {
+    return PageView.builder(
+      physics: const BouncingScrollPhysics(),
+      scrollDirection: Axis.vertical,
+      itemCount: videos.length,
+      itemBuilder: (context, index) {
+        final VideoPost videoPost = videos[index];
+        return Stack(children: [
+          // VideoPlayer + gradient
+
+          // buttons: likes, comments, share, etc.
+          Positioned(
+              bottom: 80, right: 20, child: VideoButtons(videoPost: videoPost)),
+        ]);
+      },
+    );
+  }
+}
